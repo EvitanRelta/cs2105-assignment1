1e4048f15898f22ea27ecf1c2e91940381bf973e
EvitanRelta <shauntanzongzhi@gmail.com>
2023-09-24 16:44:51 +0800

    Draft 2, with TCP buffer/chunk handling

--- WebServer-A0235143N.py
+++ WebServer-A0235143N.py
@@ -23,31 +23,16 @@ def main():
         while True:
             connection, address = s.accept()
             with connection:
-                data = connection.recv(1024)
-                if not data:
-                    break
-                req = SimpleHTTPRequest.decode(data)
-
-                prefix, key = os.path.dirname(req.path), os.path.basename(req.path)
-                assert prefix[0] == "/" and prefix.count("/") == 1
-                assert "/" not in key
-
-                res: SimpleHTTPResponse = None  # type:ignore
-                match prefix:
-                    case "/key":
-                        res = handle_key_requests(
-                            req, key, key_value_store, counter_store
-                        )
-                    case "/counter":
-                        res = handle_counter_requests(
-                            req, key, key_value_store, counter_store
-                        )
-                    case "_":
-                        raise Exception(
-                            f'Expected prefix to be "/key" or "/counter", got {prefix}'
-                        )
-
-                connection.send(res.encode())
+                buffer = b""
+                while True:
+                    chunk = connection.recv(1024)
+                    if not chunk:
+                        break  # Client disconnected
+                    buffer += chunk
+                    req_list, buffer = SimpleHTTPRequest.process_chunk(chunk)
+                    for req in req_list:
+                        res = handle_request(req, key_value_store, counter_store)
+                        connection.send(res.encode())
 
 
 # ==============================================================================
@@ -55,6 +40,26 @@ def main():
 # ==============================================================================
 def get_port() -> int:
     return int(sys.argv[1])
+
+
+def handle_request(
+    req: "SimpleHTTPRequest",
+    key_value_store: dict[str, bytes],
+    counter_store: dict[str, int],
+) -> "SimpleHTTPResponse":
+    prefix, key = os.path.dirname(req.path), os.path.basename(req.path)
+    assert prefix[0] == "/" and prefix.count("/") == 1
+    assert "/" not in key
+
+    res: SimpleHTTPResponse = None  # type: ignore
+    match prefix:
+        case "/key":
+            res = handle_key_requests(req, key, key_value_store, counter_store)
+        case "/counter":
+            res = handle_counter_requests(req, key, key_value_store, counter_store)
+        case "_":
+            raise Exception(f'Expected prefix to be "/key" or "/counter", got {prefix}')
+    return res
 
 
 def handle_key_requests(
@@ -174,10 +179,13 @@ class SimpleHTTPRequest:
     headers: dict[str, str]
     body: bytes
 
-    @staticmethod
-    def decode(encoded_req: bytes) -> "SimpleHTTPRequest":
-        raw_header, raw_body = encoded_req.decode().split("  ", 1)
-        header_fields = raw_header.split(" ")
+    def _with_body(self, body: bytes) -> "SimpleHTTPRequest":
+        self.body = body
+        return self
+
+    @classmethod
+    def _decode_headers_only(cls, raw_header: bytes) -> "SimpleHTTPRequest":
+        header_fields = raw_header.decode().split(" ")
         assert len(header_fields) >= 2
 
         method = header_fields[0].upper()  # Case-insensitive
@@ -191,8 +199,35 @@ class SimpleHTTPRequest:
             headers=dict(
                 zip(optional_header_substrings[0::2], optional_header_substrings[1::2])
             ),
-            body=raw_body.encode(),
+            body=b"",
         )
+
+    @classmethod
+    def process_chunk(cls, chunk: bytes) -> tuple[list["SimpleHTTPRequest"], bytes]:
+        req_list: list[SimpleHTTPRequest] = []
+        splitted_chunk = chunk.split(b"  ", 1)
+        while len(splitted_chunk) > 1:
+            raw_header, rest = splitted_chunk
+            bodiless_req = cls._decode_headers_only(raw_header)
+
+            # Request has complete header, with no additional body.
+            if "Content-Length" not in bodiless_req.headers:
+                req_list.append(bodiless_req)
+                splitted_chunk = rest.split(b"  ", 1)
+                continue
+
+            # Last request's body is incomplete.
+            body_len = int(bodiless_req.headers["Content-Length"])
+            if len(rest) < body_len:
+                return req_list, raw_header + rest
+
+            # Request has complete header + body.
+            raw_body, rest = rest[:body_len], rest[body_len:]
+            req = bodiless_req._with_body(raw_body)
+            req_list.append(req)
+            splitted_chunk = rest.split(b"  ", 1)
+
+        return req_list, splitted_chunk[0]
 
 
 @dataclass
